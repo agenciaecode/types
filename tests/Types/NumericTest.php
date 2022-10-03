6d9480f5bc8656a75018a9f316dc0b8b1911f82b
Mayko Kioschi <mkioschi@gmail.com>
2022-10-03 18:10:34 -0300

    Implements Numeric test

---
+++ tests/Types/NumericTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Mkioschi\Tests\Types;
+
+use Mkioschi\Types\Numeric;
+use PHPUnit\Framework\TestCase;
+
+class NumericTest extends TestCase
+{
+    public function test_should_be_able_to_create_a_valid_str()
+    {
+        $this->assertInstanceOf(Numeric::class, Numeric::from(123));
+        $this->assertEquals('123', (string)Numeric::from(123));
+        $this->assertEquals(true, Numeric::from(123)->equals(Numeric::from(123)));
+        $this->assertEquals(false, Numeric::from(123)->equals(Numeric::from(321)));
+        $this->assertEquals(123, Numeric::tryFrom(123)->getValue());
+        $this->assertEquals(123, Numeric::innFrom(123)->getValue());
+        $this->assertEquals(null, Numeric::innFrom(null));
+        $this->assertEquals(true, Numeric::isValid(123));
+        $this->assertEquals(true, Numeric::isValid(123.45));
+        $this->assertEquals(false, Numeric::isValid('123'));
+        $this->assertEquals(false, Numeric::isValid(['host' => 'github.com']));
+    }
+}
