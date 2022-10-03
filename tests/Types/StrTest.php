3576e0433d8864281d8306a4cb7ea348f4494f78
Mayko Kioschi <mkioschi@gmail.com>
2022-10-03 18:01:13 -0300

    Implements Str test

---
+++ tests/Types/StrTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Mkioschi\Tests\Types;
+
+use Mkioschi\Types\Str;
+use PHPUnit\Framework\TestCase;
+
+class StrTest extends TestCase
+{
+    public function test_should_be_able_to_create_a_valid_str()
+    {
+        $this->assertInstanceOf(Str::class, Str::from('Some string'));
+        $this->assertEquals('Some string', (string)Str::from('Some string'));
+        $this->assertEquals(true, Str::from('Some string')->equals(Str::from('Some string')));
+        $this->assertEquals(false, Str::from('Some string')->equals(Str::from('Another string')));
+        $this->assertEquals('Some string', Str::tryFrom('Some string')->getValue());
+        $this->assertEquals('Some string', Str::innFrom('Some string')->getValue());
+        $this->assertEquals(null, Str::innFrom(null));
+        $this->assertEquals(false, Str::isValid(123));
+        $this->assertEquals(false, Str::isValid(['host' => 'github.com']));
+        $this->assertEquals(true, Str::isValid('Some string'));
+        $this->assertEquals('1123', Str::extractNumbers('Some string 1: 123'));
+    }
+}
