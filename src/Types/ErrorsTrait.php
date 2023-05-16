d7beb577b08bcb927c0514b558dc295257b82723
Mayko Kioschi <mkioschi@gmail.com>
2023-05-15 22:13:04 -0300

    Refactors ErrorsTrait

--- src/Types/ErrorsTrait.php
+++ src/Types/ErrorsTrait.php
@@ -4,23 +4,13 @@ namespace Ecode\Types;
 
 trait ErrorsTrait
 {
-    /**
-     * @var string[]
-     */
     protected array $errors = [];
 
-    /**
-     * @param string $error
-     * @return void
-     */
     protected function addError(string $error): void
     {
         $this->errors[] = $error;
     }
 
-    /**
-     * @return ?string[]
-     */
     protected function getErrors(): ?array
     {
         if (count($this->errors) === 0) {
@@ -30,9 +20,6 @@ trait ErrorsTrait
         return $this->errors;
     }
 
-    /**
-     * @return bool
-     */
     protected function hasErrors(): bool
     {
         return count($this->errors) > 0;
