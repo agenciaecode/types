6a1b02263bcb17317bfdc62a78c5b5201967c313
Mayko Kioschi <mkioschi@gmail.com>
2023-05-15 22:14:40 -0300

    Refactors NumberInt and Numeric types

--- src/Types/NumberInt.php
+++ src/Types/NumberInt.php
@@ -4,18 +4,11 @@ namespace Ecode\Types;
 
 final class NumberInt extends Numeric
 {
-    /**
-     * @param int $value
-     */
     protected function __construct(int $value)
     {
         parent::__construct($value);
     }
 
-    /**
-     * @param mixed $value
-     * @return bool
-     */
     public static function isValid(mixed $value): bool
     {
         return is_int($value);
--- src/Types/Numeric.php
+++ src/Types/Numeric.php
@@ -6,41 +6,23 @@ use Exception;
 
 class Numeric
 {
-    /**
-     * @var float|int
-     */
     public readonly float|int $value;
 
-    /**
-     * @param float|int $value
-     */
     protected function __construct(float|int $value)
     {
         $this->value = $value;
     }
 
-    /**
-     * @param mixed $value
-     * @return bool
-     */
     public static function isValid(mixed $value): bool
     {
         return is_float($value) || is_int($value);
     }
 
-    /**
-     * @param float|int $value
-     * @return static
-     */
     public static function from(float|int $value): static
     {
         return new static($value);
     }
 
-    /**
-     * @param float|int $value
-     * @return ?static
-     */
     public static function tryFrom(float|int $value): ?static
     {
         try {
@@ -50,40 +32,25 @@ class Numeric
         }
     }
 
-    /**
-     * @param float|int|null $value
-     * @return ?static
-     */
     public static function innFrom(float|int|null $value): ?static
     {
-        if (is_null($value)) return null;
+        if (is_null($value)) {
+            return null;
+        }
+
         return new static($value);
     }
 
-    /**
-     * @return string
-     */
     public function __toString(): string
     {
         return (string)$this->value;
     }
 
-    /**
-     * @param self $value
-     * @return bool
-     */
     public function equals(self $value): bool
     {
         return $this->value === $value->value;
     }
 
-    /**
-     * @param float $value
-     * @param int $decimalPlaces
-     * @param string $decimalSeparator
-     * @param string $thousandsSeparator
-     * @return string
-     */
     public static function numberFormat(
         float $value,
         int $decimalPlaces = 2,
@@ -99,12 +66,6 @@ class Numeric
         );
     }
 
-    /**
-     * @param int $decimalPlaces
-     * @param string $decimalSeparator
-     * @param string $thousandsSeparator
-     * @return string
-     */
     public function format(
         int $decimalPlaces = 2,
         string $decimalSeparator = '.',
